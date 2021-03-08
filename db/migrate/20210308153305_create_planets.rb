470fc2bf6a9368eb27f0eeb45243da6d9ec7b66f
coleman gidcumb <colemangidcumb@gmail.com>
2021-03-08 08:36:12 -0700

    planet model added

---
+++ db/migrate/20210308153305_create_planets.rb
@@ -0,0 +1,14 @@
+class CreatePlanets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :planets do |t|
+      t.string :name
+      t.string :description
+      t.string :address
+      t.integer :price
+      t.integer :guest_limit
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
