5ad9b4b83a12a8fc05a7615977f223ad1662c0f1
Adrien Bellucci <adrien.bellucci60@gmail.com>
2021-03-09 16:17:53 +0100

    done with the styling of the show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@ Planet.destroy_all
 User.destroy_all
   user = User.create!(email: 'blabla@gmail.com', password: '123456')
 1.times  do
-  Planet.create!(name: 'Mars', description: 'red', address: '123 mars avenue', user_id: user.id, price: 100, guest_limit: 1_000_000)
+  Planet.create!(name: 'Mars', description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.', address: '123 mars avenue', user_id: user.id, price: 100, guest_limit: 1_000_000)
   puts 'Creating planet....'
 end
