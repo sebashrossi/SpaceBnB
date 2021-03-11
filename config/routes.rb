42c577e0ee4cf7f1d2e040238be09535e6d0e9ad
coleman gidcumb <colemangidcumb@gmail.com>
2021-03-11 07:37:13 -0700

    backend for success page completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :planets do
-    resources :bookings, only: [:new, :create, :show]
+    resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: [:index]
+  resources :bookings, only: [:index, :show]
 end
